c972cbe6a460029417999d95a82a8f38b17b5aa5
Eddie LI <14104647ls3@gmail.com>
2026-01-03 22:50:09 -0500

    feat: reroute routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
-import { Box, Heading, Button } from "@chakra-ui/react";
-import { Routes, Route, Link } from "react-router-dom";
-import TaskTable from "./components/TaskTable";
+import { Routes, Route, Navigate } from "react-router-dom";
 import UploadPage from "./pages/UploadPage";
-
-function Home() {
-  return (
-    <Box maxW={1000} mx="auto" px={6} pt={24} fontSize="sm">
-      <Box display="flex" justifyContent="space-between" alignItems="center" mb={10}>
-        <Heading>TanStack Table</Heading>
-        <Button as={Link} to="/upload" colorScheme="blue">
-          Upload CSV
-        </Button>
-      </Box>
-      <TaskTable />
-    </Box>
-  );
-}
+import ViewPage from "./pages/ViewPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigate to="/upload" replace />} />
       <Route path="/upload" element={<UploadPage />} />
+      <Route path="/view/:id" element={<ViewPage />} />
     </Routes>
   );
 }
